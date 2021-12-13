6f18136bf4ec1a158323deead9ae6beb2aa09c85
Neto <fpottinger.marques@gmail.com>
2021-12-13 16:37:34 +0000

    added touch in/out function to track in journey

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,12 +1,33 @@
 class Oystercard
-  attr_reader :balance
+  attr_reader :balance, :limit, :in_journey
+
+  DEFAULT_BALANCE = 0
+  DEFAULT_LIMIT = 90
 
   def initialize
-    @balance = balance 
+    @balance = DEFAULT_BALANCE
+    @limit = DEFAULT_LIMIT
   end
 
   def top_up(money)
+    raise "Max limit of #{@limit} reached" if @balance + money > @limit
     @balance += money
+  end
+
+  def deduct(money)
+    @balance -= money
+  end
+
+  def in_journey?
+    @in_journey
+  end
+
+  def touch_in
+    @in_journey = true
+  end
+
+  def touch_out
+    @in_journey = false
   end
 end
 
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -8,4 +8,33 @@ describe Oystercard do
   it "tops up the balance" do 
     expect(subject).to respond_to(:top_up)
   end
+
+  it "tops up the balance by 5" do  
+    expect { subject.top_up(5) }.to change { subject.balance }.from(0).to(5)
+  end
+
+  it "prevents top up above max limit" do
+    expect {subject.top_up(91)}.to raise_error "Max limit of #{subject.limit} reached"
+  end
+
+  it "deducts balance by amount" do
+    subject.top_up(5)
+    expect { subject.deduct(5) }.to change { subject.balance }.from(5).to(0)
+  end
+
+  it "checks if you are in_journey" do
+    expect(subject).to respond_to(:in_journey?)
+  end
+
+  it "touches in" do
+    subject.touch_in
+    expect(subject.in_journey?).to eq true
+  end
+
+  it "touches out" do
+    subject.touch_out
+    expect(subject.in_journey?).to eq false
+  end
+
+  # touch_in -----> @in_journey == true -----> touch_out -----> @in_journey == false
 end
